4b99ae2217d2aee2c5067592694dad2f4337adbb
han.zh <a>
2020-12-07 22:04:44 +0800

    ok

--- web/project/cache/1a56687da72a3cbe80d90bf7b3e607c2d9dfe4cf.win.tpl.php
+++ web/project/cache/1a56687da72a3cbe80d90bf7b3e607c2d9dfe4cf.win.tpl.php
@@ -12,10 +12,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'nocache_hash' => '166230810459c54f8f7c9ef0-49413878',
   'cache_lifetime' => 120,
   'version' => 'Smarty-3.1.16',
-  'unifunc' => 'content_5ed60b40c09933_41685445',
+  'unifunc' => 'content_5fbcd8e045b147_21838884',
   'has_nocache_code' => false,
 ),true); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_5ed60b40c09933_41685445')) {function content_5ed60b40c09933_41685445($_smarty_tpl) {?>﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php if ($_valid && !is_callable('content_5fbcd8e045b147_21838884')) {function content_5fbcd8e045b147_21838884($_smarty_tpl) {?>﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
