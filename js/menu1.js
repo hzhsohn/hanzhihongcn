821d29c28ab7c07545983b967b7f73cfba6db242
hzh <hzh.888@qq.com>
2024-02-13 22:23:32 +0800

    ok

--- js/menu1.js
+++ js/menu1.js
@@ -7,7 +7,6 @@ var shortcut = [
 [4,"Android程序","images/i/android.png","./project/android.php",800,600],
 [5,"Windows程序","images/i/win.png","./project/win.php",800,600],
 [6,"陈年旧证","images/i/cert.png","./zhenshu/index.php",800,730],
-[7,"我的简历","CV/cv.png","./CV",800,600],
-[8,"我的微信","weixin/weixinpay.jpg","./weixin",430,530],
-[9,"我的支付宝","alipay/icon.png","./alipay",600,620]
+[7,"我的简历","CV/cv.png","./CV/index.html",800,600],
+[8,"NAS管理","images/i/nas.png","./js/jump.html?http://"+window.location.hostname+":5000",220,200]
 ];
