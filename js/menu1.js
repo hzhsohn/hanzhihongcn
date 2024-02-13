dbc2a47e40a8ecd56c91bc56f354951a4d0fc005
hzh <hzh.888@qq.com>
2024-02-13 23:04:12 +0800

    Signed-off-by: hzh <hzh.888@qq.com>

--- js/menu1.js
+++ js/menu1.js
@@ -1,12 +1,12 @@
 ﻿//id,iconName,iconUrl,url,width,height
 var shortcut = [
 [0,"我的笔记","images/i/icon11.png","./my",850,600],
-[1,"在线聊天","images/i/chat.png","./chat",430,430],
-[2,"听个歌呗","images/i/icon36.png","./music",650,490],
-[3,"IOS程序","images/i/ios.png","./project/ios.php",800,600],
-[4,"Android程序","images/i/android.png","./project/android.php",800,600],
-[5,"Windows程序","images/i/win.png","./project/win.php",800,600],
-[6,"陈年旧证","images/i/cert.png","./zhenshu/index.php",800,730],
-[7,"我的简历","CV/cv.png","./CV/index.html",800,600],
-[8,"NAS管理","images/i/nas.png","./js/jump.html?http://"+window.location.hostname+":5000",220,200]
+[1,"NAS管理","images/i/nas.png","./js/jump.html?http://"+window.location.hostname+":5000",220,200],
+[2,"在线聊天","images/i/chat.png","./chat",430,430],
+[3,"听个歌呗","images/i/icon36.png","./music",650,490],
+[4,"IOS程序","images/i/ios.png","./project/ios.php",800,600],
+[5,"Android程序","images/i/android.png","./project/android.php",800,600],
+[6,"Windows程序","images/i/win.png","./project/win.php",800,600],
+[7,"陈年旧证","images/i/cert.png","./zhenshu/index.php",800,730],
+[8,"我的简历","CV/cv.png","./CV/index.html",800,600]
 ];
